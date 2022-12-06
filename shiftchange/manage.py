3a3567d182bdda9a9ac2983ff3c2f17d247354e0
Jared Dobry <jdobry@grierforensics.com>
2022-12-06 14:36:05 -0500

    Add missing poetry dependencies

--- shiftchange/manage.py
+++ shiftchange/manage.py
@@ -2,5 +2,8 @@ from shiftchange import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
+def main():
     app.run(host='0.0.0.0', port=5001, debug=True)
+
+if __name__ == '__main__':
+    main()
