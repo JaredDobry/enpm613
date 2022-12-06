7945c07d9eebe6f9c3d9861196ae90a1c4c27118
Jared Dobry <jdobry@grierforensics.com>
2022-12-06 14:56:55 -0500

    Point the build at the server when in production mode

--- shiftchange-ui/src/api.tsx
+++ shiftchange-ui/src/api.tsx
@@ -1,4 +1,5 @@
-export const BASE_URL = "";
+export const BASE_URL =
+  process.env.NODE_ENV === "development" ? "" : "http://127.0.0.1:5001";
 export const REGISTER_URL = `${BASE_URL}/register`;
 export const LOGIN_URL = `${BASE_URL}/login`;
 export const COMMENT_URL = `${BASE_URL}/comment`;
