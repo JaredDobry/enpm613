59c333c4f439d43106d1a0968db81e554fabb254
Jared Dobry <jdobry@grierforensics.com>
2022-12-06 15:35:52 -0500

    LoginSchema

--- shiftchange/resources/user.py
+++ shiftchange/resources/user.py
@@ -1,15 +1,21 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
-from flask_jwt_extended import create_access_token, get_jwt_identity, create_refresh_token, get_jwt, jwt_required
+from flask_jwt_extended import (
+    create_access_token,
+    get_jwt_identity,
+    create_refresh_token,
+    get_jwt,
+    jwt_required,
+)
 
 from db import db
 from models import UserModel
-from schemas import UserSchema
-
+from schemas import UserSchema, LoginSchema
 
 
 bp = Blueprint("Users", "users", description="Operations on users")
+
 
 @bp.route("/add_user")
 class Add_user(MethodView):
@@ -28,9 +34,9 @@ class Add_user(MethodView):
 
         return {"message": "User created successfully."}, 201
 
+
 @bp.route("/user/<int:user_id>")
 class User(MethodView):
-
     @bp.response(200, UserSchema)
     def get(self, user_id):
         user = UserModel.query.get_or_404(user_id)
@@ -42,19 +48,20 @@ class User(MethodView):
         db.session.commit()
         return {"message": "User deleted."}, 200
 
+
 @bp.route("/login")
 class UserLogin(MethodView):
-    @bp.arguments(UserSchema)
-    def post(self, user_data):
-        user = UserModel.query.filter(
-            UserModel.username == user_data["username"]
-        ).first()
+    @bp.arguments(LoginSchema)
+    def post(self, login: LoginSchema):
+        user = UserModel.query.filter(UserModel.username == login["username"]).first()
 
-        if user and pbkdf2_sha256.verify(user_data["password"], user.password):
+        if user and pbkdf2_sha256.verify(login["password"], user.password):
             access_token = create_access_token(identity=user.id, fresh=True)
             refresh_token = create_refresh_token(user.id)
-            UserModel.user_type=user_data["user_type"]
-            return {"access_token": access_token, "refresh_token": refresh_token, "user_type": UserModel.user_type}, 200
+            return {
+                "access_token": access_token,
+                "refresh_token": refresh_token,
+            }, 200
             # access_token = create_access_token(identity=user.id)
             # return {"access_token": access_token}, 200
 
